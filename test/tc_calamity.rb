9586373f5e6da4a5a1e88b50327fd01b57e63e0f
Kevin Beddingfield <kevinbeddingfield@Kevins-MacBook-Air.local>
2012-03-20 20:33:10 -0700

    testing db creation from main executable

--- test/tc_calamity.rb
+++ test/tc_calamity.rb
@@ -2,13 +2,16 @@ require 'test/unit'
 require 'date'
 
 class TC_testCalamity < Test::Unit::TestCase
-
   def setup
+    `rm ./test/calamity.db`
   end
 
   def teardown
+    `rm ./test/calamity.db`
   end
 
-  def test_add_default_date
+  def test_create_db
+    `./bin/calamity --database="./test/calamity.db" add foo`
+    assert File.exists?('./test/calamity.db')
   end
 end
